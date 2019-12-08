9a9ff997b5f456d5004303cc077e3bb47699855a
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2019-12-08 18:04:17 +0700

    fix

--- frontend/controllers/MembersController.php
+++ frontend/controllers/MembersController.php
@@ -21,35 +21,38 @@ use appxq\sdii\helpers\SDHtml;
 class MembersController extends Controller
 {
 
-    public function beforeAction($action) {
-	if (parent::beforeAction($action)) {
-	    if (in_array($action->id, array('create', 'update','delete','index'))) {
-		
-	    }
-	    return true;
-	} else {
-	    return false;
-	}
+    public function beforeAction($action)
+    {
+        if (parent::beforeAction($action)) {
+            if (in_array($action->id, array('create', 'update', 'delete', 'index'))) {
+
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
-    
+
     /**
      * Lists all Members models.
      * @return mixed
      */
-    public function actionGetBooking(){
+    public function actionGetBooking()
+    {
         $id = Yii::$app->request->get('id');
-        $booking = Booking::find()->where('id=:id',[
-            ':id'=>$id
+        $booking = Booking::find()->where('id=:id', [
+            ':id' => $id
         ])->one();
         $html = "";
-        if($booking){
-            $date = isset($booking->date)?SDdate::mysql2phpDate($booking->date):'';
-            $time = isset($booking->time)?$booking->time:'';
+        if ($booking) {
+            $date = isset($booking->date) ? SDdate::mysql2phpDate($booking->date) : '';
+            $time = isset($booking->time) ? $booking->time : '';
 
             $html .= "วันที่จัดอบรม {$date} เวลา {$time}";
         }
         return $html;
     }
+
     public function actionIndex()
     {
         $searchModel = new MembersSearch();
@@ -68,15 +71,15 @@ class MembersController extends Controller
      */
     public function actionView($id)
     {
-	if (Yii::$app->getRequest()->isAjax) {
-	    return $this->renderAjax('view', [
-		'model' => $this->findModel($id),
-	    ]);
-	} else {
-	    return $this->render('view', [
-		'model' => $this->findModel($id),
-	    ]);
-	}
+        if (Yii::$app->getRequest()->isAjax) {
+            return $this->renderAjax('view', [
+                'model' => $this->findModel($id),
+            ]);
+        } else {
+            return $this->render('view', [
+                'model' => $this->findModel($id),
+            ]);
+        }
     }
 
     /**
@@ -86,26 +89,30 @@ class MembersController extends Controller
      */
     public function actionCreate()
     {
-	if (Yii::$app->getRequest()->isAjax) {
-	    $model = new Members();
+        if (Yii::$app->getRequest()->isAjax) {
+            $model = new Members();
 
-	    if ($model->load(Yii::$app->request->post())) {
-            $model->rstat = 1;
-            $model->create_date = date('Y-m-d H:i:s');
-            $model->create_by = isset(\Yii::$app->user->id)?\Yii::$app->user->id:'';
-		if ($model->save()) {
-		    return \cpn\chanpan\classes\CNMessage::getSuccess('Create successfully');
-		} else {
-		    return \cpn\chanpan\classes\CNMessage::getError('Can not create the data.');
-		}
-	    } else {
-		return $this->renderAjax('create', [
-		    'model' => $model,
-		]);
-	    }
-	} else {
-	    throw new NotFoundHttpException('Invalid request. Please do not repeat this request again.');
-	}
+            if ($model->load(Yii::$app->request->post())) {
+                $model->rstat = 1;
+                $model->create_date = date('Y-m-d H:i:s');
+                $model->create_by = isset(\Yii::$app->user->id) ? \Yii::$app->user->id : '';
+                if ($model->save()) {
+                    return \cpn\chanpan\classes\CNMessage::getSuccess('Create successfully');
+                } else {
+                    return \cpn\chanpan\classes\CNMessage::getError('Can not create the data.');
+                }
+            } else {
+                $model->fname = isset(Yii::$app->user->identity->profile->firstname)?Yii::$app->user->identity->profile->firstname:'';
+                $model->lname = isset(Yii::$app->user->identity->profile->lastname)?Yii::$app->user->identity->profile->lastname:'';
+                $model->tel = isset(Yii::$app->user->identity->profile->tel)?Yii::$app->user->identity->profile->tel:'';
+                $model->email = isset(Yii::$app->user->identity->profile->public_email)?Yii::$app->user->identity->profile->public_email:'';
+                return $this->renderAjax('create', [
+                    'model' => $model,
+                ]);
+            }
+        } else {
+            throw new NotFoundHttpException('Invalid request. Please do not repeat this request again.');
+        }
     }
 
     /**
@@ -116,26 +123,26 @@ class MembersController extends Controller
      */
     public function actionUpdate($id)
     {
-	if (Yii::$app->getRequest()->isAjax) {
-	    $model = $this->findModel($id);
+        if (Yii::$app->getRequest()->isAjax) {
+            $model = $this->findModel($id);
 
-	    if ($model->load(Yii::$app->request->post())) {
-        $model->rstat = 1;
-        $model->update_date = date('Y-m-d H:i:s');
-        $model->update_by = isset(\Yii::$app->user->id)?\Yii::$app->user->id:'';
-		if ($model->save()) {
-		    return \cpn\chanpan\classes\CNMessage::getSuccess('Update successfully');
-		} else {
-		    return \cpn\chanpan\classes\CNMessage::getError('Can not update the data.');
-		}
-	    } else {
-		return $this->renderAjax('update', [
-		    'model' => $model,
-		]);
-	    }
-	} else {
-	    throw new NotFoundHttpException('Invalid request. Please do not repeat this request again.');
-	}
+            if ($model->load(Yii::$app->request->post())) {
+                $model->rstat = 1;
+                $model->update_date = date('Y-m-d H:i:s');
+                $model->update_by = isset(\Yii::$app->user->id) ? \Yii::$app->user->id : '';
+                if ($model->save()) {
+                    return \cpn\chanpan\classes\CNMessage::getSuccess('Update successfully');
+                } else {
+                    return \cpn\chanpan\classes\CNMessage::getError('Can not update the data.');
+                }
+            } else {
+                return $this->renderAjax('update', [
+                    'model' => $model,
+                ]);
+            }
+        } else {
+            throw new NotFoundHttpException('Invalid request. Please do not repeat this request again.');
+        }
     }
 
     /**
@@ -146,43 +153,44 @@ class MembersController extends Controller
      */
     public function actionDelete($id)
     {
-	if (Yii::$app->getRequest()->isAjax) {
-        $model = $this->findModel($id);
-        $model->rstat = 3;
-        $model->update_date = date('Y-m-d H:i:s');
-        $model->update_by = isset(\Yii::$app->user->id)?\Yii::$app->user->id:'';
-	    if ($model->save()) {
-
-		    return \cpn\chanpan\classes\CNMessage::getSuccess('Delete successfully');
-	    } else {
-		    return \cpn\chanpan\classes\CNMessage::getError('Can not delete the data.');
-	    }
-	} else {
-	    throw new NotFoundHttpException('Invalid request. Please do not repeat this request again.');
-	}
-    }
-
-    public function actionDeletes() {
-	if (Yii::$app->getRequest()->isAjax) {
-
-	    if (isset($_POST['selection'])) {
-		foreach ($_POST['selection'] as $id) {
-		    $model = $this->findModel($id);
+        if (Yii::$app->getRequest()->isAjax) {
             $model = $this->findModel($id);
             $model->rstat = 3;
             $model->update_date = date('Y-m-d H:i:s');
-            $model->update_by = isset(\Yii::$app->user->id)?\Yii::$app->user->id:'';
-            $model->save();
-		}
-		return \cpn\chanpan\classes\CNMessage::getSuccess('Delete successfully'); 
-	    } else {
-		return \cpn\chanpan\classes\CNMessage::getError('Can not delete the data.'); 
-	    }
-	} else {
-	    throw new NotFoundHttpException('Invalid request. Please do not repeat this request again.');
-	}
+            $model->update_by = isset(\Yii::$app->user->id) ? \Yii::$app->user->id : '';
+            if ($model->save()) {
+
+                return \cpn\chanpan\classes\CNMessage::getSuccess('Delete successfully');
+            } else {
+                return \cpn\chanpan\classes\CNMessage::getError('Can not delete the data.');
+            }
+        } else {
+            throw new NotFoundHttpException('Invalid request. Please do not repeat this request again.');
+        }
     }
-    
+
+    public function actionDeletes()
+    {
+        if (Yii::$app->getRequest()->isAjax) {
+
+            if (isset($_POST['selection'])) {
+                foreach ($_POST['selection'] as $id) {
+                    $model = $this->findModel($id);
+                    $model = $this->findModel($id);
+                    $model->rstat = 3;
+                    $model->update_date = date('Y-m-d H:i:s');
+                    $model->update_by = isset(\Yii::$app->user->id) ? \Yii::$app->user->id : '';
+                    $model->save();
+                }
+                return \cpn\chanpan\classes\CNMessage::getSuccess('Delete successfully');
+            } else {
+                return \cpn\chanpan\classes\CNMessage::getError('Can not delete the data.');
+            }
+        } else {
+            throw new NotFoundHttpException('Invalid request. Please do not repeat this request again.');
+        }
+    }
+
     /**
      * Finds the Members model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
